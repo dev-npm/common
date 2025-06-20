4b72603dc4d83904b1bfaac6b805b4d37e6a907e
colors9 <saifumar9@gmail.com>
2025-06-19 20:04:58 -0700

    Update june19-menu-role.ts

--- june19-menu-role.ts
+++ june19-menu-role.ts
@@ -139,4 +139,97 @@ public class ReportsController : ControllerBase
   </li>
 
 </nz-menu>
+neeeeeeeeeee
+
+@Component({ selector: 'app-header', templateUrl: './header.component.html' })
+export class HeaderComponent implements OnInit {
+  reportMenusGrouped: {
+    [customerType: string]: { standard: AppReport[]; custom: AppReport[] };
+  } = {};
+
+  constructor(
+    private reportService: ReportService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const role = this.authService.getUserRole();
+
+    this.reportService.getReportsByRole(role).subscribe(reports => {
+      this.reportMenusGrouped = {};
+
+      for (const rpt of reports) {
+        const type = rpt.customer_type_name;
+
+        if (!this.reportMenusGrouped[type]) {
+          this.reportMenusGrouped[type] = { standard: [], custom: [] };
+        }
+
+        if (rpt.is_custom_report) {
+          this.reportMenusGrouped[type].custom.push(rpt);
+        } else {
+          this.reportMenusGrouped[type].standard.push(rpt);
+        }
+      }
+    });
+  }
+
+  get customerTypes(): string[] {
+    return Object.keys(this.reportMenusGrouped);
+  }
+
+  openReport(report: AppReport): void {
+    this.router.navigate(['/reports', report.route_path], {
+      state: {
+        reportId: report.id,
+        customerTypeId: report.customer_type_id,
+        isCustom: report.is_custom_report
+      }
+    });
+  }
+}
+<nz-menu nzMode="horizontal">
+
+  <!-- Dynamic Report Menus -->
+  <ng-container *ngFor="let type of customerTypes">
+    <li nz-submenu [nzTitle]="type + ' Reports'">
+      <ul>
+        <ng-container *ngIf="reportMenusGrouped[type].standard.length > 0">
+          <li class="menu-section-header">Standard</li>
+          <li nz-menu-item *ngFor="let rpt of reportMenusGrouped[type].standard"
+              (click)="openReport(rpt)">
+            {{ rpt.report_name }}
+          </li>
+        </ng-container>
+
+        <ng-container *ngIf="reportMenusGrouped[type].custom.length > 0">
+          <li class="menu-section-header">Custom</li>
+          <li nz-menu-item *ngFor="let rpt of reportMenusGrouped[type].custom"
+              (click)="openReport(rpt)">
+            {{ rpt.report_name }}
+          </li>
+        </ng-container>
+      </ul>
+    </li>
+  </ng-container>
+
+  <!-- Static Admin Settings -->
+  <li nz-submenu nzTitle="Admin Settings">
+    <ul>
+      <li nz-menu-item routerLink="/user-management">User Management</li>
+      <li nz-menu-item routerLink="/role-permissions">Role Permissions</li>
+    </ul>
+  </li>
+
+  <!-- Static Project Settings -->
+  <li nz-submenu nzTitle="Project Settings">
+    <ul>
+      <li nz-menu-item routerLink="/project-list">Projects</li>
+      <li nz-menu-item routerLink="/project-config">Configuration</li>
+      <li nz-menu-item routerLink="/milestones">Milestones</li>
+    </ul>
+  </li>
+
+</nz-menu>
 
