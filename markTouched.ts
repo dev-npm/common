ff7fb47d38a7c40773fc42bab0d3c17ed822d785
colors9 <saifumar9@gmail.com>
2025-09-21 16:10:03 -0700

    Add markAllAsTouched and markControlsDirty methods

--- markTouched.ts
+++ markTouched.ts
@@ -18,3 +18,30 @@ markAllAsTouched(control: FormGroup | FormArray) {
     });
   }
 }
+
+
+import { AbstractControl, FormGroup, FormArray, FormControl } from '@angular/forms';
+
+markAllAsTouched(control: AbstractControl): void {
+  if (control instanceof FormControl) {
+    control.markAsTouched({ onlySelf: true });
+  } else if (control instanceof FormGroup) {
+    Object.keys(control.controls).forEach(key => {
+      this.markAllAsTouched(control.controls[key]);
+    });
+  } else if (control instanceof FormArray) {
+    control.controls.forEach(c => this.markAllAsTouched(c));
+  }
+}
+
+markControlsDirty(control: AbstractControl): void {
+  if (control instanceof FormControl) {
+    control.markAsDirty({ onlySelf: true });
+    control.updateValueAndValidity({ onlySelf: true });
+  } else if (control instanceof FormGroup) {
+    Object.values(control.controls).forEach(c => this.markControlsDirty(c));
+  } else if (control instanceof FormArray) {
+    control.controls.forEach(c => this.markControlsDirty(c));
+  }
+}
+
